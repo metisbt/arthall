049d0f1ab3ac51b2a2d3d9d3df901bc2ab73a2b7
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-03-06 03:44:54 +0330

    added art models
    + added Category, Creation, Exhibition, Teaching and RegisterExhibition class

--- art/models.py
+++ art/models.py
@@ -1,3 +1,74 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length = 255)
+
+    def __str__(self):
+        return self.name
+
+class Creation(models.Model):
+    title = models.CharField(max_length = 255)
+    content = models.TextField()
+    image = models.ImageField(upload_to='art/',default='art/default.jpg')
+    category = models.ManyToManyField(Category)
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    counted_view = models.IntegerField(default=0)
+    status = models.BooleanField(default=False)
+    login_require = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
+
+    class Meta:
+
+        ordering = ['-created_date']
+
+    def __str__ (self):
+        return self.title
+    
+class Exhibition(models.Model):
+    name = models.CharField(max_length = 255)
+    category = models.ManyToManyField(Category)
+    proved = models.BooleanField(default=False)
+    date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        ordering = ['-created_date']
+
+    def __str__ (self):
+        return self.name
+    
+class Teaching(models.Model):
+    name = models.CharField(max_length = 255)
+    subject = models.CharField(max_length = 255)
+    category = models.ManyToManyField(Category)
+    proved = models.BooleanField(default=False)
+    date = models.DateTimeField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        ordering = ['-created_date']
+
+    def __str__ (self):
+        return self.name
+    
+class RegisterExhibition(models.Model):
+    name = models.ForeignKey(Exhibition, on_delete=models.SET_NULL, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        ordering = ['-created_date']
+
+    def __str__ (self):
+        return self.name
