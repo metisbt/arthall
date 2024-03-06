3fe20ce4c7a9fd93d94e739304236dc06f107766
Mohamad Mahdi Sabet <77541798+metisbt@users.noreply.github.com>
2024-03-06 03:50:25 +0330

    Update admin.py

--- art/admin.py
+++ art/admin.py
@@ -1,3 +1,31 @@
 from django.contrib import admin
+from art.models import Category, Creation, Exhibition, Teaching, RegisterExhibition
 
-# Register your models here.
+@admin.register(Creation)
+class CreationAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('title', 'author', 'counted_view', 'status', 'login_require', 'published_date', 'created_date')
+    list_filter = ('status','author')
+    search_fields = ['title', 'content']
+
+@admin.register(Exhibition)
+class ExhibitionAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name', 'category', 'proved', 'date', 'created_date', 'updated_date')
+    list_filter = ('name','category', 'proved', 'date')
+    search_fields = ['name', 'proved', 'date']
+
+@admin.register(Teaching)
+class TeachingAdmin(admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    empty_value_display = '-empty-'
+    list_display = ('name', 'category', 'subject', 'proved', 'date', 'created_date', 'updated_date')
+    list_filter = ('name','category', 'proved', 'date')
+    search_fields = ['name', 'proved', 'date']
+
+
+admin.site.register(Category)
+admin.site.register(RegisterExhibition)
+
